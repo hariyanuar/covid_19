eca2387f7910d89b593b6d0b46f7d4dae1ec78f1
Harry76 <hariyanuar.hy1@gmail.com>
2020-04-09 11:18:27 +0700

    - Added image classification feature - Improved questions grammar - Added copyright

--- lib/main.dart
+++ lib/main.dart
@@ -30,37 +30,50 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(
-            "assets/icons/coronavirus.png",
-            scale: 2,
-          ),
-          SizedBox(
-            height: 25.0,
-          ),
-          Text(
-            "Ayo cari tahu apakah kehidupanmu bisa membuat kamu tertular virus Corona!",
-            style: Theme.of(context).textTheme.title,
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(
-            height: 25.0,
-          ),
-          RaisedButton(
-            child: Text("MULAI", style: TextStyle(color: Colors.white, fontSize: 32),),
-            padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 12.0),
-            color: Theme.of(context).primaryColor,
-            onPressed: () {
-              Navigator.pushNamed(context, "RegistrationPage");
-            },
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  "assets/icons/coronavirus.png",
+                  scale: 2,
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                Text(
+                  "Ayo cari tahu apakah kehidupanmu bisa membuat kamu tertular virus Corona!",
+                  style: Theme.of(context).textTheme.title,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                RaisedButton(
+                  child: Text(
+                    "MULAI",
+                    style: TextStyle(color: Colors.white, fontSize: 32),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 12.0),
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () {
+                    Navigator.pushNamed(context, "RegistrationPage");
+                  },
+                ),
+              ],
+            ),
+            Positioned(
+              child: Text("Hari Yanuar\nGEN-16 RPL\nSMK Wikrama\n2020", style: Theme.of(context).textTheme.subhead.copyWith(color: Colors.grey), textAlign: TextAlign.center,),
+              bottom: 0,
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
--- lib/result_page.dart
+++ lib/result_page.dart
@@ -9,35 +9,63 @@ class ResultPage extends StatelessWidget {
     answers = ModalRoute.of(context).settings.arguments as Map<int, bool>;
     int yesAnswer = answers.values.where((bool answer) => answer).length;
     String result;
+    String classificationIcon;
 
-    if (yesAnswer <= 7)
+    if (yesAnswer <= 7) {
       result = "Rendah";
-    else if (yesAnswer <= 14)
+      classificationIcon = "healthy.png";
+    } else if (yesAnswer <= 14) {
       result = "Sedang";
-    else if (yesAnswer > 14) result = "Tinggi";
-
+      classificationIcon = "cautious.png";
+    } else if (yesAnswer > 14) {
+      result = "Tinggi";
+      classificationIcon = "risky.png";
+    }
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(50.0),
         alignment: Alignment.center,
-        child: RichText(
-          textAlign: TextAlign.center,
-          text: TextSpan(
-            text: Biodata.nama + ',\n\n',
-            style: Theme.of(context).textTheme.title.copyWith(fontSize: 48),
-            children: [
-              TextSpan(
-                  text: "Resiko anda untuk tertular penyakit COVID-19 adalah ",
-                  style: Theme.of(context)
-                      .textTheme
-                      .title
-                      .copyWith(fontWeight: FontWeight.normal),
-                  children: <TextSpan>[
-                    TextSpan(
-                        text: result, style: Theme.of(context).textTheme.title),
-                  ])
-            ]
-          ),
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset("assets/icons/$classificationIcon"),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Text(
+                  Biodata.nama + ',',
+                  style: Theme.of(context).textTheme.title.copyWith(fontSize: 48),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    text: "Resiko anda untuk tertular penyakit COVID-19 adalah ",
+                    style: Theme.of(context)
+                        .textTheme
+                        .title
+                        .copyWith(fontWeight: FontWeight.normal),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: result, style: Theme.of(context).textTheme.title),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              child: Text("#DiRumahAja", style: Theme.of(context).textTheme.subhead.copyWith(color: Colors.grey),),
+              bottom: 0,
+            ),
+          ],
         ),
         //child: Text("Resiko anda untuk tertular penyakit COVID-19 adalah ${result}", style: Theme.of(context).textTheme.title, textAlign: TextAlign.center,),
       ),
